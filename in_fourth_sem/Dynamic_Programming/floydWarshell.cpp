15904374ec586118f49fa0ea53dd42e7e4a56161
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-02-24 11:17:28 +0530

    The floyd warshell algo is implemeted

---
+++ in_fourth_sem/Dynamic_Programming/floydWarshell.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printMatrix(vector<vector<int>>&m);
+vector<vector<int>>Matrix(int n);
+vector<vector<int>>FloydWarshell(int n , vector<vector<int>>adj);
+int main(){
+    vector<vector<int>>adj = Matrix(4);
+    cout << endl << endl;
+    printMatrix(adj);
+    vector<vector<int>>res = FloydWarshell(4, adj);
+    cout << endl << endl;
+    printMatrix(res);
+}
+
+vector<vector<int>> FloydWarshell(int n , vector<vector<int>>adj){
+    for(int k = 1 ; k <= n ; k++ ){
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=n ; j++){
+
+                // formula to modify matrix, if distance is less with itermediate vertex.
+                if(adj[i][j] > adj[i][k] + adj[k][j]){
+                    adj[i][j] = adj[i][k] + adj[k][j];
+                }
+
+
+            }
+        }
+    }
+    return adj;
+}
+
+
+
+
+
+// to create 2d matrix with user input 
+vector<vector<int>>Matrix(int n){
+    vector<vector<int>>m(n+1, vector<int>(n+1,-1));
+    // printMatrix(m);
+    for(int i = 1 ; i<=n ; i++){
+        cout << "Enter  vlaue to add element into row " << i << endl;
+        for(int j = 1 ; j<=n ; j++){
+            cin >> m[i][j];
+        }
+
+    }
+    return m;
+}
+
+// to print adjaceny matrix
+void printMatrix(vector<vector<int>>&m){
+    // we are ignoring the first 0 index
+    for(int i = 1 ; i< m.size(); i++){
+        for(int j = 1 ; j<m.size() ; j++){
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
