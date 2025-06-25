b28823cdc7c6712a81a6e7fa2539c1cdbbf19b43
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-06-25 18:42:46 +0530

    leetcode problem 300 is solved using memoization.

--- in_fourth_sem/some-leetcode-problem/Dynamic Programming/include-exclude/300_increasing_longest_subsequence.cpp
+++ in_fourth_sem/some-leetcode-problem/Dynamic Programming/include-exclude/300_increasing_longest_subsequence.cpp
@@ -33,11 +33,28 @@ public:
         // }
         return max(a,b);
     }
+
+    int lengthOfLIS2(vector<int>& nums) {
+        vector<int>dp(nums.size(), 1);     // dp[i] represent the LIS ending at i, not a gloabal max carried forward.
+        int maxLIS = 1;         // for globally 
+        for(int i = 0 ; i < nums.size() ; i++){
+
+            for(int j = 0 ; j < i ; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                    maxLIS = max(maxLIS,dp[i]);         // update gloabl variable if required
+                }
+            }
+        }
+        return maxLIS;
+        
+    }
 };
 
 int main(){
     vector<int>nums = {0, 1, 0, 3, 2, 3};
     Solution s; 
-    cout << s.lengthOfLIS(nums);
+    cout << s.lengthOfLIS(nums)<< endl;
+    cout << s.lengthOfLIS2(nums);
     
 }
