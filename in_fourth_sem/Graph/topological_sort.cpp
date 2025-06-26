17b97257af0ab779d537174163c10369bedda719
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-06-26 17:14:08 +0530

    topological sort is solved using kahn's algo(bfs)

--- in_fourth_sem/Graph/topological_sort.cpp
+++ in_fourth_sem/Graph/topological_sort.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+#include<queue>
 using namespace std;
+
+vector<vector<int>>edgesIntoAdjacencyList(int V, vector<vector<int>>& edges){
+        vector<vector<int>>adj(V);
+        for(int i = 0 ; i < edges.size() ; i++ ){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+        }
+        return adj;
+}
+
 
 class Solution {
   public:
@@ -9,12 +21,7 @@ class Solution {
         // code here
         
         // convert into adjacency list
-        vector<vector<int>>adj(V);
-        for(int i = 0 ; i < edges.size() ; i++ ){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back(v);
-        }
+        vector<vector<int>>adj = edgesIntoAdjacencyList(V, edges);
         
         vector<bool>visited(V, false);
         stack<int>st;        // for topologically ordering
@@ -51,14 +58,56 @@ class Solution {
         st.push(u);           // u vertex comes before v ; u -> v
         // cout << "hi";
     }
+
+    vector<int>topoSortUsingBFS(int V, vector<vector<int>>& edges){     // kahns algo
+        // convert int adjacency list
+        vector<vector<int>>adj = edgesIntoAdjacencyList(V,edges);
+
+        // counting indegree of each vertex
+        vector<int>inDeg(V,0);
+        for(int u = 0 ;  u< adj.size(); u++){
+            for(int v : adj[u]){
+                inDeg[v]++;
+            }
+        }
+        // for bfs traversal but insertion of vertex with 0 indeg into queue
+        queue<int>q;
+
+        // finding indeg 0 vertex
+        for(int u = 0 ; u < inDeg.size() ; u++){
+            if(inDeg[u] == 0){
+                q.push(u);
+            }
+        }
+        vector<int>result;      // storing resultant topological sort
+        while(!q.empty()){
+            int u = q.front();
+            result.push_back(u);
+            q.pop();
+            for(int v : adj[u]){
+                inDeg[v]--;
+                if(inDeg[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+
+        return result;
+    }
 };
 
 int main(){
     int v = 4;
     vector<vector<int>>edges = {{3, 0}, {1,0},  {2,0}};
     Solution s;
-    vector<int>topo_sort = s.topoSort(v, edges);
-    for(int i : topo_sort){
+    vector<int>topo_sort1 = s.topoSort(v, edges);
+    vector<int>topo_sort2 = s.topoSortUsingBFS(v,edges);
+ 
+    for(int i : topo_sort1){
+        cout << i << " ";
+    }
+    cout << endl << endl;
+    for(int i : topo_sort1){
         cout << i << " ";
     }
     
