c02dd78792da3d8889510a9968c5531d488a1c8a
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-06-26 17:37:50 +0530

    detecting cycle in DG is solved using BFS which is sames as Kahn's algo

--- in_fourth_sem/Graph/detect_cycle_in_dg.cpp
+++ in_fourth_sem/Graph/detect_cycle_in_dg.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
+
+// convert edges list to adjacency list
+vector<vector<int>>edgesIntoAdjacencyList(int V, vector<vector<int>>& edges){
+        vector<vector<int>>adj(V);
+        for(int i = 0 ; i < edges.size() ; i++ ){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+        }
+        return adj;
+}
+
 void print2Vector(vector<vector<int>>& );
 class Solution {
     public:
       bool isCyclic(int V, vector<vector<int>> &edges) {
           // convert edges list to adjacency list
-          vector<vector<int>> adj(V);
-          for(int i = 0 ; i < edges.size() ; i++){
-                int u = edges[i][0];
-                int v = edges[i][1];
-                adj[u].push_back(v);
+          vector<vector<int>> adj = edgesIntoAdjacencyList(V, edges);
 
-          }
-          
-          print2Vector(adj);
+        //   print2Vector(adj);
         
         // vector for visited nodes whether they are visited or not
         vector<bool>visited(V,false);
@@ -47,6 +54,51 @@ class Solution {
             }
             return false;
       }
+
+
+
+      // this is totally same as kahn's algo (topological sort using bfs)
+      bool solveUsingBFS(int V, vector<vector<int>>& edges){
+        vector<vector<int>>adj = edgesIntoAdjacencyList(V, edges);
+        //counting indegree of each vertex
+        vector<int>inDeg(V,0);
+        for(int u = 0 ;  u< adj.size(); u++){
+            for(int v : adj[u]){
+                inDeg[v]++;
+            }
+        }
+        // for bfs traversal but insertion of vertex with 0 indeg into queue
+        queue<int>q;
+
+        // finding indeg 0 vertex
+        int count = 0;
+        for(int u = 0 ; u < inDeg.size() ; u++){
+            if(inDeg[u] == 0){
+                q.push(u);
+                count++;
+            }
+        } 
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int v : adj[u]){
+                inDeg[v]--;
+                if(inDeg[v] == 0){
+                    count++;
+                    q.push(v);
+                }
+            }
+        }
+        
+        // extra from kahn's algo
+        if(count == V){         // topological sort exist i.e. graph is DAG.
+            return false;
+        }else{
+            return true;
+        }
+
+      }
+
 };
 
 
@@ -56,6 +108,8 @@ int main(){
     // edges = {{0,1},{2,1}};
     Solution s;
     cout << s.isCyclic(v,edges);
+    cout << endl << endl;
+    cout << s.solveUsingBFS(v,edges);
 }
 
 void print2Vector(vector<vector<int>>& adj){
